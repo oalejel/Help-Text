3e33370985676a419466245468ffe1de81ac64d8
oalejel <alejel.omar@gmail.com>
2017-06-07 07:18:44 -0400

    Initial Commit

---
+++ Help TextTests/Help_TextTests.swift
@@ -0,0 +1,36 @@
+//
+//  Help_TextTests.swift
+//  Help TextTests
+//
+//  Created by Omar Al-Ejel on 6/7/17.
+//  Copyright © 2017 Omar Al-Ejel. All rights reserved.
+//
+
+import XCTest
+@testable import Help_Text
+
+class Help_TextTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
